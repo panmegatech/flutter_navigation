90497ce1500263c0e1d0c0725920a0138cc02487
pandamegatech <panmegatech@gmail.com>
2024-10-02 02:23:10 +0700

    feat: router navigation system

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation/ant_screen.dart';
+import 'package:flutter_navigation/app_router.dart';
 import 'package:flutter_navigation/bee_screen.dart';
 import 'package:flutter_navigation/cat_screen.dart';
 import 'package:flutter_navigation/home_screen.dart';
@@ -23,43 +24,9 @@ class MainApp extends StatelessWidget {
       //   "/bee": (context) => const BeeScreen(),
       //   "/cat": (context) => const CatScreen(),
       // },
+
       initialRoute: RouteName.home,
-      onGenerateRoute: (settings) {
-        final MaterialPageRoute? pageRoute;
-        final routeName = settings.name;
-
-        switch (routeName) {
-          case RouteName.ant:
-            //todo ant logic
-            pageRoute = MaterialPageRoute(
-              builder: (context) => const AntScreen(),
-            );
-            break;
-          case RouteName.bee:
-            pageRoute = MaterialPageRoute(
-              builder: (context) => const BeeScreen(),
-            );
-            break;
-          case RouteName.cat:
-            pageRoute = MaterialPageRoute(
-              builder: (context) => const CatScreen(),
-            );
-            break;
-          case RouteName.home:
-            pageRoute = MaterialPageRoute(
-              builder: (context) => const HomeScreen(),
-            );
-            break;
-
-          case RouteName.pageNotFound:
-          default:
-            pageRoute = MaterialPageRoute(
-              builder: (context) => const PageNotFoundScreen(),
-            );
-        }
-
-        return pageRoute;
-      },
+      onGenerateRoute: AppRouter.generateRoute,
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
           builder: (context) => const PageNotFoundScreen(),
